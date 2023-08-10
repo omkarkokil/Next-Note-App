23a0b90285e886afc11bcac2a7bd7bf91ba64231
omkarkokil <nsjustice21@gmail.com>
2023-08-10 11:37:44 +0530

    base bugfix -1

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { withAuth } from "next-auth/middleware"
 export default withAuth({
     pages: {
         // signIn: "/",
-        signIn: "https://addit.vercel.app",
+        signIn: "https://addit.vercel.app/",
     },
 })
 
